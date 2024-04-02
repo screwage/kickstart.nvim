9ebd5694b0be2abece1f7e92967873035909857c
screwage <code@screwage.net>
2024-04-01 20:29:45 -0400

    initial migration

---
+++ lua/dave/set.lua
@@ -0,0 +1,33 @@
+vim.opt.guicursor = ''
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
+
+vim.opt.updatetime = 50
+
+-- Use system clipboard
+vim.opt.clipboard = 'unnamed'
+
+vim.g.mapleader = ' '
+
+vim.opt.hlsearch = true
+
+vim.opt.cursorline = true
+
+vim.g['tmux_navigator_no_mappings'] = 1
